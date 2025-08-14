5c35c904d2201e26326e5ec5b7e505cfffeb3c80
Cerith Freeman <cerith.elis@gmail.com>
2025-08-14 15:19:07 +0100

    .

--- website/pages/feedback.py
+++ website/pages/feedback.py
@@ -1,14 +1,17 @@
 import streamlit as st
 from website.local_functions import *
 
-def feedback_response(question, options, key, number_labels=True):
+def feedback_response(question, options, key, label_type):
     
-    if number_labels:
+    if label_type == "numbers":
         labels=['1','5']
         width = 5
-    else:
+    elif label_type == "agreement":
         labels=['Disagree', 'Agree']
         width = 60
+    else:
+        labels = label_type
+        width = int(len(labels[0])*10)
 
     st.markdown(f'**{question}**')
     with st.container(horizontal=True, horizontal_alignment="left", width=300):
@@ -37,21 +40,46 @@ with st.form("feedback_form"):
     overall_rating = feedback_response(
         question="How would you rate _Wrth Sgwrs_ overall?",
         options="stars",
-        key="overall_rating"
-    )
-
-    ease_of_use = feedback_response(
-        question="How much do you agree with the statement: _Wrth Sgwrs_ is easy to use and navigate?",
-        options="faces",
-        key="ease_of_use",
-        number_labels=False
+        key="overall_rating",
+        label_type="numbers"
     )
 
     improves_welsh = feedback_response(
         question="Do you think _Wrth Sgwrs_ would help you improve your Welsh?",
         options="faces",
-        key="improves_welsh"
+        key="improves_welsh",
+        label_type="numbers"
     )
+
+    ease_of_use = feedback_response(
+        question="How much do you agree with the statement: \"_Wrth Sgwrs_ is easy to use and navigate.\"?",
+        options="faces",
+        key="ease_of_use",
+        label_type="agreement"
+    )
+
+    freedom = feedback_response(
+        question="How much do you agree with the statement: \"I appreciate the freedom the app gives me to learn independently.\"?",
+        options="faces",
+        key="freedom",
+        label_type="agreement"
+    )
+
+    structure = feedback_response(
+        question="How much do you agree with the statement: \"The app could use more structure or guidance so I know what to do\"?",
+        options="faces",
+        key="structure",
+        label_type="agreement"
+    )
+
+    resources = feedback_response(
+        question="Is the convenience of having multiple resources in one place important to you when learning a language?",
+        options="faces",
+        key="resources",
+        label_type=['Not at all', 'Yes, very']
+    )
+
+
 
     st.divider() # --------------------
 
