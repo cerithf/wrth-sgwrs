a0a887017fcdf27f7a419029c7ed8ab208fc9803
Cerith Freeman <cerith.elis@gmail.com>
2025-08-15 16:38:28 +0100

    .

--- website/local_functions.py
+++ website/local_functions.py
@@ -14,8 +14,11 @@ db_connection = st.connection("gsheets", type=GSheetsConnection)
 user_db = db_connection.read(worksheet="Users", usecols=[1])
 db_users = user_db[user_db.columns[0]].to_list()
 
-st.session_state["cookie_controller"] = CookieController()
-cc = st.session_state["cookie_controller"]
+if "cookie_controller" not in st.session_state:
+    st.session_state["cookie_controller"] = CookieController()
+    cc = st.session_state["cookie_controller"]
+else:
+    cc = st.session_state["cookie_controller"]
 
 # GENERAL
 
