6c2e04b763e7643a4ee412485944b908959e18bd
Cerith Freeman <cerith.elis@gmail.com>
2025-08-17 20:26:38 +0100

    .

--- website/local_functions.py
+++ website/local_functions.py
@@ -399,11 +399,11 @@ def load_user_topics():
 
 def save_ai_response(input, exemplar, response):
     df = db_connection.read(worksheet="Test_Yourself", ttl=0)
-    data = {
+    data = [{
         'input': input,
         'exemplar': exemplar,
         'response': response,
         'timestamp': dt.datetime.now().strftime('%d-%m-%Y %X')
-    }
+    }]
     df = pd.concat([df,pd.DataFrame(data)], ignore_index=True)
     db_connection.update(worksheet="Test_Yourself", data=df)
