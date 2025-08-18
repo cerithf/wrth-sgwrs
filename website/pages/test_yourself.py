7e497f99bed269805214dfdeaa2be606e0275ffa
Cerith Freeman <cerith.elis@gmail.com>
2025-08-18 13:17:58 +0100

    .

--- website/pages/test_yourself.py
+++ website/pages/test_yourself.py
@@ -24,8 +24,6 @@ for i in range(len(columns)):
             if st.button(topic['en'], key=f'topic_{topic['index']}', icon=topic['emoji'], use_container_width=True): #type: ignore
                 ss.chosen_topic = topic
 
-
-
 if ss.chosen_topic:
     st.write('_Here\'s a paragraph in English based on the topic you chose. Try and write the Welsh translation in the box below and see how close you get! Feel free to only tackle part of the paragraph for now, you can always come back to it later._')
     st.divider()
@@ -38,12 +36,14 @@ if ss.chosen_topic:
 
     with st.form("form"):
         submitted_text = st.text_area("Write")
-        l,r = st.columns(2)
-        with l: submit_exemplar = st.form_submit_button('Submit (Exemplar)', use_container_width=True)
-        with r: submit_no_exemplar = st.form_submit_button('Submit (No Exemplar)',  use_container_width=True)
+        a,b,c = st.columns(3)
+        with a: submit_1 = st.form_submit_button('Submit (Exemplar+English)', use_container_width=True)
+        with b: submit_2 = st.form_submit_button('Submit (English)',  use_container_width=True)
+        with c: submit_3 = st.form_submit_button('Submit (Translation Only)',  use_container_width=True)
 
-    def modify_prompt(paragraph=paragraph, translation=submitted_text, exemplar=True) -> str:
-        if exemplar:
+
+    def modify_prompt(prompt_type, paragraph=paragraph, translation=submitted_text) -> str:
+        if prompt_type=="exemplar_and_english":
             return f'''
         I am a user who is learning Welsh. The following is a paragraph written in English that I have been asked to translate into Welsh. Please compare the two and tell me where I have made any mistakes or if there are any improvements that can be made. Use the paragraph under the heading "exemplar" to guide your feedback. If I make a mistake regarding mutation, just refer to it as "mutation" rather than "soft mutation", "aspirate mutation", or "nasal mutation", as you often incorrectly label what kind of mutation is present. My translation may only cover part of the original paragraph: this is okay, be encouraging (no need to ask me to do the whole thing). Also remember that I cannot see the exemplar, so do not tell me to refer to it.
 
@@ -56,7 +56,7 @@ if ss.chosen_topic:
         Exemplar:
         {paragraph['cy']}
         '''
-        else:
+        elif prompt_type=="english_provided":
             return f'''
         I am a Welsh learner. I have been asked to translate a paragraph from English into Welsh. I'll share the original English paragraph and then the Welsh translation. Please tell me how I can improve my translation.
 
@@ -66,6 +66,16 @@ if ss.chosen_topic:
         My translation:
         {translation}
         '''
+        elif prompt_type=="translation_only":
+            return f'''
+        I am a Welsh learner. I have been asked to translate a paragraph from English into Welsh. Please give me feedback on my translation.
+
+        My translation:
+        {translation}
+        '''
+        else:
+            return f'There has been an error.'
+        
     # -----
 
     # Setting OpenAI API key from Streamlit secrets
@@ -86,15 +96,20 @@ if ss.chosen_topic:
         except:
             pass
 
-    if submit_exemplar or submit_no_exemplar:
+    if (submit_1 or submit_2 or submit_3):
 
-        if submit_exemplar:
-            prompt = modify_prompt(exemplar=True)
+        if submit_1:
+            prompt_type = "exemplar_and_english"
+        elif submit_2:
+            prompt_type = "english_provided"
+        elif submit_3:
+            prompt_type = "translation_only"
         else:
-            prompt =  modify_prompt(exemplar=False)
+            prompt_type = "error"
+    
 
         # Add user message to chat history
-        st.session_state.test_messages.append({"role": "user", "content": prompt})
+        st.session_state.test_messages.append({"role": "user", "content": modify_prompt(prompt_type)})
 
         # Display assistant response in chat message container
         with st.chat_message(name="assistant",avatar="👨🏼‍🏫"):
@@ -108,4 +123,4 @@ if ss.chosen_topic:
         st.session_state.test_messages.append({"role": "assistant", "content": response})
 
         if ss.sub == 'guest_cerith':
-            save_ai_response(submitted_text,paragraph['topic'],response, model=st.session_state['openai_model'], prompt=prompt)
+            save_ai_response(submitted_text,paragraph['topic'],response, model=st.session_state['openai_model'], prompt=prompt_type)
