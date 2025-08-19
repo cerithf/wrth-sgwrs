96dc75f1b7ba7ebd2a45294ada0f2add13c1a1bc
Cerith Freeman <cerith.elis@gmail.com>
2025-08-19 20:28:10 +0100

    .

--- website/local_functions.py
+++ website/local_functions.py
@@ -365,6 +365,7 @@ def save_user_topics(input_topics):
         df = pd.concat([df, pd.DataFrame(data)], ignore_index=True)
     else:
         i = df[df['user_id'] == user_id].index
+        st.write('i is:', i)
         df.loc[i, 'topics'] = topics
         df.loc[i, 'last_updated'] = now
     
